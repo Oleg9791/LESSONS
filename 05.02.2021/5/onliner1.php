484e7841df339c5a228cdd4be2b3d3f8f4e22fc2
Oleg9791 <oleg9791@tut.by>
2021-02-05 21:24:18 +0300

    ch

--- 05.02.2021/5/onliner1.php
+++ 05.02.2021/5/onliner1.php
@@ -2,4 +2,6 @@
 
 $data = file_get_contents("https://www.onliner.by");
 $pat = "/\bМинск\b/iu";
-echo preg_match_all($pat, $data);
+$mtch=[];
+echo preg_match_all($pat, $data,$match);
+print_r($match);
