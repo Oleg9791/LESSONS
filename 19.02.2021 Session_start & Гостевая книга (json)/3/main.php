d7a75c280a9e6d57d2b2a41bf9124ccb44317aca
Oleg9791 <oleg9791@tut.by>
2021-02-25 21:57:48 +0300

    changes

--- 19.02.2021 Session_start & Гостевая книга (json)/3/main.php
+++ 19.02.2021 Session_start & Гостевая книга (json)/3/main.php
@@ -7,24 +7,41 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 
     <title>Document</title>
 </head>
 <body>
-<?php
-include "smile.php";
+<br>
+<div class="container">
+    <div class="row">
+        <div class="col-sm">
+            One of three columns
+        </div>
+        <div class="col-sm">
+            <?php
+            include "smile.php";
 
-$data = json_decode(file_get_contents("data.json"), true);
-foreach ($data as $row) {
-    if (!cenzura($row["msg"])) {
+            $data = json_decode(file_get_contents("data.json"), true);
+            echo "<table border='1' class='table table-bordered border-primary'>";
+            foreach ($data as $row) {
+                if (!cenzura($row["msg"])) {
 
-        $row1 = preg_replace("/\b\d{2}+\-\d{2}+\-\d{2}+\b/ius", " ", $row["time"]);
+                    $row1 = preg_replace("/\b\d{2}+\-\d{2}+\-\d{2}+\b/ius", " ", $row["time"]);
+                    $fun = smile($row["msg"]);
+                    echo "<tr><td>$fun</td><td>$row[name]</td><td>$row1</td></tr>";
+                }
 
-        echo smile($row["msg"]) . " " . $row["name"] . $row1 . "<br>\n";
-    }
+            }
+            echo "</table>";
+            ?>
 
-}
-?>
+        </div>
+        <div class="col-sm">
+            One of three columns
+        </div>
+    </div>
+</div>
 <div class="container">
     <div class="row">
         <div class="col-sm">
@@ -32,8 +49,7 @@ foreach ($data as $row) {
         </div>
         <div class="col-sm">
             <form action="add.php" method="post">
-    <textarea name="msg" class="form-control" placeholder="Leave a comment here" id="floatingTextarea2"
-              style="height: 100px" cols="30" rows="10"></textarea>
+                <textarea name="msg" class="form-control" cols="30" rows="10"></textarea><br>
                 <input type="text" name="name">
                 <input type="submit" class="btn btn-primary" value="ok">
             </form>
