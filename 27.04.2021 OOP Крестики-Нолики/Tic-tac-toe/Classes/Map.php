8ef29a2fb581bc66aeddc0c0decb5d97606ea40f
Oleg9791 <oleg9791@tut.by>
2021-04-28 22:07:39 +0300

    added 28.04.2021

---
+++ 27.04.2021 OOP Крестики-Нолики/Tic-tac-toe/Classes/Map.php
@@ -0,0 +1,44 @@
+<?php
+
+
+class Map extends TicTac
+{
+    /**
+     * @param array $map
+     * @return Map
+     */
+    public function setMap(array $map): static
+    {
+        $this->map = $map;
+        return $this;
+    }
+
+    public function getHtmlTable(): string
+    {
+        $table = "";
+
+        for ($i = 0; $i < count($this->map); $i++) {
+            $table .= "<table border='1'>";
+
+            for ($j = 0; $j < count($this->map); $j++) {
+                if ($this->map[$i][$j] == "X") {
+                    $table .= "<td style='width: 30px;height: 30px'>X</td>";
+
+                } elseif ($this->map[$i][$j] == "0") {
+                    $table .= "<td style='width: 30px;height: 30px'>0</td>";
+
+                }
+
+                if ($this->map[$i][$j] == "") {
+                    $table .= "<td style='width: 30px;height: 30px'></td>";
+                }
+
+            }
+
+        }
+        $table .= "</table>";
+
+        return $table;
+    }
+
+}
--- 27.04.2021 OOP Крестики-Нолики/Tic-tac-toe/test.php
+++ 27.04.2021 OOP Крестики-Нолики/Tic-tac-toe/test.php
@@ -5,69 +5,78 @@ include "unittest.php";
 
 $tictac = new TicTac();
 
-//print_r($tictac
-//    ->init(2)
-//    ->putCross(1, 1)
-//    ->getMap()
+($tictac
+    ->init(3)
+    ->putCross(1, 1)
+    ->putZero(2,2)
+    ->putZero(1,2)
+    ->putCross(2, 1)
+
+    ->getMap()
+);
+
+$map = new Map();
+echo $map
+    ->setMap($tictac->getMap())
+    ->getHtmlTable();
+//test(
+//    $tictac->init(2)->getMap(),
+//    [
+//        ["", ""],
+//        ["", ""]
+//    ]
 //);
-test(
-    $tictac->init(2)->getMap(),
-    [
-        ["", ""],
-        ["", ""]
-    ]
-);
-
-
-test(
-    $tictac->init(3)->getMap(),
-    [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""]
-    ]
-);
-
-
-test(
-    $tictac->init(1)->getMap(),
-    [
-        [""],
-    ]
-);
-
-test(
-    $tictac->init(3)->putCross(1, 2)->getMap(),
-    [
-        ["", "", ""],
-        ["", "", "X"],
-        ["", "", ""]
-    ]
-);
-
-test(
-    $tictac->init(3)->putCross(2, 2)->getMap(),
-    [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", "X"]
-    ]
-);
-
-test(
-    $tictac->init(3)->putZero(2, 2)->getMap(),
-    [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", "0"]
-    ]
-);
-
-test(
-    $tictac->init(3)->putZero(1, 2)->getMap(),
-    [
-        ["", "", ""],
-        ["", "", "0"],
-        ["", "", ""]
-    ]
-);
+//
+//
+//test(
+//    $tictac->init(3)->getMap(),
+//    [
+//        ["", "", ""],
+//        ["", "", ""],
+//        ["", "", ""]
+//    ]
+//);
+//
+//
+//test(
+//    $tictac->init(1)->getMap(),
+//    [
+//        [""],
+//    ]
+//);
+//
+//test(
+//    $tictac->init(3)->putCross(1, 2)->getMap(),
+//    [
+//        ["", "", ""],
+//        ["", "", "X"],
+//        ["", "", ""]
+//    ]
+//);
+//
+//test(
+//    $tictac->init(3)->putCross(2, 2)->getMap(),
+//    [
+//        ["", "", ""],
+//        ["", "", ""],
+//        ["", "", "X"]
+//    ]
+//);
+//
+//test(
+//    $tictac->init(3)->putZero(2, 2)->getMap(),
+//    [
+//        ["", "", ""],
+//        ["", "", ""],
+//        ["", "", "0"]
+//    ]
+//);
+//
+//test(
+//    $tictac->init(3)->putZero(1, 2)->getMap(),
+//    [
+//        ["", "", ""],
+//        ["", "", "0"],
+//        ["", "", ""]
+//    ]
+//);
