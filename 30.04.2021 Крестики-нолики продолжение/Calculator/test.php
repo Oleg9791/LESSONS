1ce947609580042c700bc8e0e9c303fb436405f1
Oleg9791 <oleg9791@tut.by>
2021-05-02 21:58:30 +0300

    chahges

--- 30.04.2021 Крестики-нолики продолжение/Calculator/test.php
+++ 30.04.2021 Крестики-нолики продолжение/Calculator/test.php
@@ -1,11 +1,11 @@
 <?php
 include "Calculator.php";
 
-$calc = new Calculator(1,4);
+$calc = new Calculator(1, 4);
 //$calc->setA(6);
 echo $calc->setA(8)->getA();
 
-echo $calc->setA(4)->setB(3)->sum();
+//echo $calc->setA(4)->setB(3)->sum();
 
 //echo $calc->sum();
 //echo $calc->setA(4)->setB(3)->sum();
