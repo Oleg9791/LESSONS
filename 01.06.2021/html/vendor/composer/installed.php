fe2b9bb9c21a5650a794286e9e390926ad0112ba
Oleg9791 <oleg9791@tut.by>
2021-06-01 23:24:17 +0300

    added html and pagination

---
+++ 01.06.2021/html/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5782e29189f19f1e2cc1b2950a1c8d7313caa326',
+    'name' => 'w1020/orm',
+  ),
+  'versions' => 
+  array (
+    'w1020/orm' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5782e29189f19f1e2cc1b2950a1c8d7313caa326',
+    ),
+  ),
+);
